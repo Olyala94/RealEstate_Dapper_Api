15a4405acedaa04eed1a1a12d1a4c474235da334
olyadev <yusupowa9494@mail.ru>
2023-09-08 09:19:10 +0300

    #21 - Müşteri Yorumları kısmı yapıldı

---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepositories/ITestimonialRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepositories
+{
+    public interface ITestimonialRepository
+    {
+        Task<List<ResultTestimonialDto>> GetAllTestimonialAsync();
+    }
+}
+
---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepositories/TestimonilalRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepositories
+{
+    public class TestimonilalRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+
+        public TestimonilalRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultTestimonialDto>> GetAllTestimonialAsync()
+        {
+            string query = "Select * From Testmonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}

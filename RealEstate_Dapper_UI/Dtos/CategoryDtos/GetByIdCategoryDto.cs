a9cedbeda314da4a333ba53c0c2f15cd8a55fde8
olyadev <yusupowa9494@mail.ru>
2023-09-15 10:25:02 +0300

    Admin Panelinde Kategori Apinin Consume Edilme işlemleri yapıldı.

---
+++ RealEstate_Dapper_UI/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.BottomGridDtos
+{
+    public class GetByIdCategoryDto
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+    }
+}

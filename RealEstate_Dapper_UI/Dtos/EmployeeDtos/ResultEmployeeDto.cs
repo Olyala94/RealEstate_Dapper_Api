e349cd5ba2bc4e391b7f0434d87b037fa8763460
olyadev <yusupowa9494@mail.ru>
2023-10-26 21:35:12 +0300

    #30 - Employee Consume işlemleri yapıldı.

--- RealEstate_Dapper_UI/Dtos/EmployeeDtos/ResultEmployeeDto.cs
+++ RealEstate_Dapper_UI/Dtos/EmployeeDtos/ResultEmployeeDto.cs
@@ -1,6 +1,6 @@
-﻿namespace RealEstate_Dapper_Api.Dtos.EmployeeDtos
+﻿namespace RealEstate_Dapper_UI.Dtos.EmployeeDtos
 {
-    public class UpdateEmployeeDto
+    public class ResultEmployeeDto
     {
         public int EmployeeID { get; set; }
         public string Name { get; set; }
@@ -8,6 +8,6 @@
         public string Mail { get; set; }
         public string PhoneNumber { get; set; }
         public string ImageUrl { get; set; }
-        public bool Status { get; set; }
+        public bool Status { get; set; } 
     }
 }

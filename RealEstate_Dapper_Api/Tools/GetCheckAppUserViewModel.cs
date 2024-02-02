dead9c1062d75642e83ba2dd08dca6a7a31b5d6f
user <user@DESKTOP-9E5P6SH>
2024-02-02 16:08:23 +0300

    Json Web Token anlatımı yapıldı (başlangıç)

---
+++ RealEstate_Dapper_Api/Tools/GetCheckAppUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class GetCheckAppUserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public bool IsExist { get; set; }  //Bu kulllanıcıyı içeriyormu , tanımlı ?
+    }
+}
---
+++ RealEstate_Dapper_Api/Tools/TokenResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class TokenResponseViewModel
+    {
+        public TokenResponseViewModel(int token, DateTime expireDate)
+        {
+            Token = token;
+            ExpireDate = expireDate;
+        }
+
+        public int Token { get; set; }
+        public DateTime ExpireDate { get; set; } 
+    }
+}

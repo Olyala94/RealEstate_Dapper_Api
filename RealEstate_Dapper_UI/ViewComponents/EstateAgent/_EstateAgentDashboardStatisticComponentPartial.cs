f4c95a05b7ef0fb1dc042f11e0ead600ffcc9764
OlyaYu <olyayusupova498@gmail.com>
2024-03-27 06:53:20 +0300

    Giriş Yapan Emlakçiye Ait Dashboard İstatistik işlemleri Yapıldı

---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentDashboardStatisticComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            #region Statistics(1) - Toplam İlan Sayisi
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:44327/api/EstateAgentDashboardStatistic/AllProductCount");
+            var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.productCount = jsonData1;
+            #endregion
+
+            #region Statistics(2) - Emlakçının Toplam İlan Sayısı
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:44327/api/EstateAgentDashboardStatistic/ProductCountByEmployeeId?id=1");
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            ViewBag.employeeByProductCount = jsonData2;
+            #endregion
+
+            #region Statistics(3) - Aktif İlan Sayısı
+            var client3 = _httpClientFactory.CreateClient();
+            var responseMessage3 = await client3.GetAsync("https://localhost:44327/api/EstateAgentDashboardStatistic/ProductCountByStatusTrue?id=1");
+            var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+            ViewBag.productCountByEmployeeStatusTrue = jsonData3;
+            #endregion
+
+            #region Statistics(4)- Pasif İlan Sayısı
+            var client4 = _httpClientFactory.CreateClient();
+            var responseMessage4 = await client4.GetAsync("https://localhost:44327/api/EstateAgentDashboardStatistic/ProductCountByStatusFalse?id=1");
+            var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+            ViewBag.productCountByEmployeeStatusFalse = jsonData4;
+            #endregion
+
+            return View();
+        }
+    }
+}
